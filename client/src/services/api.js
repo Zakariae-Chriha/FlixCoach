25d915ea295a9ce0ef2c957807727e7e6818345c
unknown <chrihazakaria@email.com>
2026-04-22 00:03:38 +0200

    Fix: use API_URL from config in axios baseURL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
+import { API_URL } from '../config';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: `${API_URL}/api`,
   headers: { 'Content-Type': 'application/json' },
 });
 
